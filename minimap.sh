44b1291c53c48bd3e0a2e0ddad47a49a4440a8f2
Franck-Dumetz <105318680+Franck-Dumetz@users.noreply.github.com>
2024-10-15 08:40:02 -0400

    Update minimap.sh

--- minimap.sh
+++ minimap.sh
@@ -3,14 +3,14 @@
 ### slurm script for minimap2
 
 #SBATCH --job-name=mgCST_classifier                       # Job name
-#SBATCH --output=/local/scratch/amaros/logs/slurm/R.out   # Standard output log
-#SBATCH --error=/local/scratch/amaros/logs/slurm/R.err    # Standard error log
+#SBATCH --output=                                         # Standard output log
+#SBATCH --error=                                          # Standard error log
 #SBATCH --ntasks=1                                        # Number of tasks
 #SBATCH --mem=25G                                         # Memory per node
 #SBATCH --partition=all                                   # Partition name
 
-fastq_file=path_to_fastq_directory/concatenated_fastq_file.fastq
+fastq_file=/path/concatenated_fastq_file.fastq
 sam_file=experiment_name.sam
-gen_ref=path_to_ref_genome.fasta
+ref_gen=/path/ref_genome.fasta
 
-minimap2 -ax map-ont -t 2 “$ref_file” “$fastq_file” > “$sam_file”
+minimap2 -ax map-ont -t 2 “$ref_gen” “$fastq_file” > “$sam_file”
